d99bacc1b270ccd62554399297f37d76e1ba83fd
marijanbrvar <marijan.brvar@gmail.com>
2021-04-17 18:39:32 +0200

    Add seeds with dumy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+9.times do |blog|
+Blog.create!(
+	title: "My Blog Title ##{blog} ", 
+	body: "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment. His many legs, pitifully thin compared with the size of the rest of him, waved about helplessly as he looked.")
+end
+
+5.times do |skill|
+	Skill.create!(
+		title: 'Rails #{skill}',
+		percent_utilized: 15 )
+end
+
+puts "9 times Blog post created"
+puts "5 times Skill created"
