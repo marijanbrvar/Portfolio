7664a71f08f0bf39d6eeebeb53b9a131b7b8467c
marijanbrvar <marijan.brvar@gmail.com>
2021-04-17 23:54:19 +0200

    Add custom portfolio pat for show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :portfolio_sets #, except: [:show]
-  # get 'portfolio/:id', to: 'portfolio_sets#show', as: 'portfolio_show'
+  resources :portfolio_sets, except: [:show]
+  get 'portfolio/:id', to: 'portfolio_sets#show', as: 'portfolio_show'
   
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
