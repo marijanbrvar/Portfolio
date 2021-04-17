662bf3e3b26017b72ca9467e4962c9c9d76c5982
marijanbrvar <marijan.brvar@gmail.com>
2021-04-17 23:06:05 +0200

    Add new and show action to Portfolio

--- app/controllers/portfolio_sets_controller.rb
+++ app/controllers/portfolio_sets_controller.rb
@@ -1,5 +1,24 @@
 class PortfolioSetsController < ApplicationController
   def index
-    @portfoilos = PortfolioSet.all
+    @portfolio_items = PortfolioSet.all
+  end
+
+  def new
+    @portfolio_item = PortfolioSet.new
+  end
+
+  def create
+    @portfolio_item = PortfolioSet.new(params.require(:portfolio_set).permit(:title, :subtitle, :body, :main_image,
+                                                                             :thumb_image))
+
+    if @portfolio_item.save
+      redirect_to portfolio_sets_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @portfolio_item = PortfolioSet.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :portfolio_sets
+  resources :portfolio_sets #, except: [:show]
+  # get 'portfolio/:id', to: 'portfolio_sets#show', as: 'portfolio_show'
   
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
