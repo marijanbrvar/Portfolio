4afd72b7feec54d8110427e87267fc066119b6cd
marijanbrvar <marijan.brvar@gmail.com>
2021-04-19 00:27:19 +0200

    Add basic authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolio_sets, except: [:show]
   get 'portfolio/:id', to: 'portfolio_sets#show', as: 'portfolio_show'
   get 'vuejs-items', to: 'portfolio_sets#vuejs'
